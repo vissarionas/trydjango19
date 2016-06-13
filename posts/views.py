6d6f8a542ea44f3cfe1f2b09bd793e005cd4a834
vissarionas <vissarionas@gmail.com>
2016-06-13 22:31:17 +0300

    urls,views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def post_create(request):
+	return HttpResponse("<H1>create</H1>")
+
+def post_detail(request):
+	return HttpResponse("<H1>detail</H1>")
+
+def post_list(request):
+	return HttpResponse("<H1>list</H1>")
+
+def post_update(request):
+	return HttpResponse("<H1>update</H1>")
+
+def post_delete(request):
+	return HttpResponse("<H1>delete</H1>")
+
