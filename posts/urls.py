36fd71caa8256ac539f7cfa9e40061c565f078c3
vissarionas <vissarionas@gmail.com>
2016-06-17 06:38:10 +0300

    dynamic urls, querysets

--- posts/urls.py
+++ posts/urls.py
@@ -4,12 +4,14 @@ from .views import (
 	post_create,
 	post_detail,
 	post_update,
-	post_delete)
+	post_delete,
+	post_populate)
 
 urlpatterns = [
     url(r'^$', post_list),
     url(r'^create/$', post_create),
-    url(r'^detail/$', post_detail),
+    url(r'^(?P<post_id>\d+)/', post_detail),
     url(r'^update/$', post_update),
-    url(r'^delete/$', post_delete),
+    url(r'^delete/(?P<post_id>\d+)/$', post_delete),
+	url(r'^populate/(?P<pop_count>\d+)/', post_populate),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from posts.models import Post
+import random
 
 # Create your views here.
 
@@ -9,19 +11,35 @@ def post_create(request):
 	else:
 		return HttpResponse("<H1>create2</H1>")
 
-def post_detail(request):
-	return HttpResponse("<H1>detail</H1>")
+def post_detail(request , post_id):
+	post = get_object_or_404(Post , id = post_id)
+	context = {
+		"post":post,
+		"title":post.title,
+	}
+	return render(request, 'post_detail.html' , context)
 
 def post_list(request):
+	#Post.objects.create(title="auto post", content="created on page load")
+	# if request.user.is_authenticated:
+	queryset = Post.objects.all()
+	# queryset = Post.objects.all()[:3]
 	context = {
-	"title":"goofy_list",
+		'title' : "Posts",
+		"posts" : queryset
 	}
-	#return HttpResponse("<H1>list</H1>")
-	return render(request, 'index.html' , context)
+
+	return render(request, 'index.html', context)
 
 def post_update(request):
 	return HttpResponse("<H1>update</H1>")
 
-def post_delete(request):
-	return HttpResponse("<H1>delete</H1>")
+def post_delete(request , post_id):
+	Post.objects.filter(id = post_id).delete()
+	return post_list(request)
 
+def post_populate(request, pop_count):
+	for i in range(int(pop_count)):
+		title = random.randint(0,500)
+		Post.objects.create(title=title, content="koukoubaounes"+str(title))
+	return post_list(request)
